36b1d1485a619963aa0c5a56ac9b178f7b6ca31e
Joel Winarske <joel.winarske@gmail.com>
2023-02-02 09:00:20 -0800

    line wrap error fix

--- recipes-vm/firecracker/firecracker/hello-firecracker.sh
+++ recipes-vm/firecracker/firecracker/hello-firecracker.sh
@@ -3,8 +3,7 @@
 arch=`uname -m`
 dest_kernel="hello-vmlinux.bin"
 dest_rootfs="hello-rootfs.ext4"
-image_bucket_url="https://s3.amazonaws.com/spec.ccfc.min/img/quickstart_guide/$a
-rch"
+image_bucket_url="https://s3.amazonaws.com/spec.ccfc.min/img/quickstart_guide/$arch"
 
 if [ ${arch} = "x86_64" ]; then
     kernel="${image_bucket_url}/kernels/vmlinux.bin"
